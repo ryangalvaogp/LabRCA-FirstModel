b41b2a5f105a716e0fee7df9a544b82438199f3d
ryangalvaogp <60650064+ryangalvaogp@users.noreply.github.com>
2020-05-07 12:52:57 -0300

    Erros corrigidos

--- backend/src/controllers/sessionControllers.js
+++ backend/src/controllers/sessionControllers.js
@@ -3,13 +3,15 @@ const connection = require ('.././database/conection');
 module.exports = {
 async create (request, response){
     const { Email } = request.body;
-    console.log(Email)
+    
     const Usuario = await connection ('usuario').where('Email', Email).select('Name', 'Email', 'id').first();
 
     if (!Usuario){
         return response.status (400).json({error: 'Usuario não encontrada pelo ID'})
     }
+    
     return response.json(Usuario)
+   
     
 
 
